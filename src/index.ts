a6d152940e2c71f3c5c4f2e63b62d5044c02f9c0
Robby6Strings <rausten93@gmail.com>
2023-10-04 09:58:30 +1300

    fix matching bug

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { match } from "./match.js"
 
-match("asd")
+match({ a: { b: 123 } })
   .with([
-    [[], () => console.log("empty array")],
-    [[1, 2, 3], () => console.log("array with values")],
-    [123, () => console.log("number")],
     [{}, () => console.log("empty object")],
+    [{ a: 123 }, () => console.log("object with a:123")],
+    [{ a: { b: 123 } }, () => console.log("object with a: { b: 123 }")],
+    [Object, () => console.log("object type")],
+    [[], () => console.log("empty array")],
+    [[1, 2, 3], () => console.log("array with values 1,2,3")],
+    [123, () => console.log("number")],
     ["", () => console.log("empty string")],
     [String, () => console.log("string type")],
     [Number, () => console.log("number type")],
+    [Error, () => console.log("error type")],
   ])
   .or(() => console.log("no match"))
   .catch((e) => console.log(e))
--- src/match.ts
+++ src/match.ts
@@ -3,175 +3,120 @@ type Pattern = [any, () => any]
 class PatternMatcher {
   matched = false
   error = null
-  constructor(x: any, patterns: Pattern[]) {
+  constructor(value: any, patterns: Pattern[]) {
     let match: Pattern | undefined
 
     for (const pattern of patterns) {
-      if (typeof x === "string") {
-        this.matched =
-          pattern[0] === String ||
-          (typeof pattern[0] === "string" && pattern[0] === x)
-        if (this.matched) {
-          match = pattern
+      const [lhs] = pattern
+      switch (typeof value) {
+        case "string":
+          this.matched =
+            lhs === String || (typeof lhs === "string" && lhs === value)
           break
-        }
-      }
-
-      if (typeof x === "number") {
-        this.matched =
-          pattern[0] === Number ||
-          (typeof pattern[0] === "number" && pattern[0] === x)
-
-        if (this.matched) {
-          match = pattern
+        case "number":
+          this.matched =
+            lhs === Number || (typeof lhs === "number" && lhs === value)
           break
-        }
-      }
-
-      if (typeof x === "boolean") {
-        this.matched =
-          pattern[0] === Boolean ||
-          (typeof pattern[0] === "boolean" && pattern[0] === x)
-
-        if (this.matched) {
-          match = pattern
+        case "boolean":
+          this.matched =
+            lhs === Boolean || (typeof lhs === "boolean" && lhs === value)
           break
-        }
-      }
-
-      if (typeof x === "bigint") {
-        this.matched =
-          pattern[0] === BigInt ||
-          (typeof pattern[0] === "bigint" && pattern[0] === x)
-
-        if (this.matched) {
-          match = pattern
+        case "bigint":
+          this.matched =
+            lhs === BigInt || (typeof lhs === "bigint" && lhs === value)
           break
-        }
-      }
-
-      if (typeof x === "symbol") {
-        this.matched =
-          pattern[0] === Symbol ||
-          (typeof pattern[0] === "symbol" && pattern[0] === x)
-
-        if (this.matched) {
-          match = pattern
+        case "symbol":
+          this.matched =
+            lhs === Symbol || (typeof lhs === "symbol" && lhs === value)
           break
-        }
-      }
-
-      if (typeof x === "function") {
-        this.matched =
-          pattern[0] === Function ||
-          (typeof pattern[0] === "function" && pattern[0] === x)
-
-        if (this.matched) {
-          match = pattern
+        case "function":
+          this.matched =
+            lhs === Function || (typeof lhs === "function" && lhs === value)
           break
-        }
-      }
-
-      if (x instanceof Date) {
-        this.matched =
-          pattern[0] === Date ||
-          (pattern[0] instanceof Date && pattern[0] === x)
-        if (this.matched) {
-          match = pattern
+        default:
           break
-        }
       }
 
-      if (x instanceof RegExp) {
-        this.matched =
-          pattern[0] === RegExp ||
-          (pattern[0] instanceof RegExp && pattern[0] === x)
-        if (this.matched) {
-          match = pattern
-          break
-        }
-      }
-
-      if (x instanceof Error) {
-        this.matched =
-          pattern[0] === Error ||
-          (pattern[0] instanceof Error && pattern[0] === x)
-        if (this.matched) {
-          match = pattern
-          break
-        }
-      }
-
-      if (x instanceof Promise) {
-        this.matched =
-          pattern[0] === Promise ||
-          (pattern[0] instanceof Promise && pattern[0] === x)
-        if (this.matched) {
-          match = pattern
-          break
-        }
-      }
-
-      if (x === null) {
-        this.matched = pattern[0] === null
-        if (this.matched) match = pattern
-        break
-      }
-
-      if (x === undefined) {
-        this.matched = pattern[0] === undefined
-        if (this.matched) match = pattern
-        break
-      }
-
-      if (Array.isArray(x)) {
-        if (!Array.isArray(pattern[0])) break
-        if (x.length === 0) {
-          this.matched = pattern[0].length === 0
-        } else if (x.length !== pattern[0].length) {
-          this.matched = false
-        } else {
-          this.matched = pattern[0].every((p, i) => p === x[i])
-        }
-
-        if (this.matched) {
-          match = pattern
-          break
-        } else {
-          continue
-        }
-      }
-
-      if (typeof x === "object") {
-        if (typeof pattern[0] !== "object") break
-        if (Object.keys(x).length === 0) {
-          this.matched = Object.keys(pattern[0]).length === 0
-        } else if (Object.keys(x).length !== Object.keys(pattern[0]).length) {
-          this.matched = false
-        } else {
-          this.matched = Object.keys(pattern[0]).every(
-            (p) => pattern[0][p] === x[p]
-          )
-        }
-        if (this.matched) {
-          match = pattern
-          break
-        } else {
-          continue
-        }
-      }
-
-      this.matched = pattern[0] === x
       if (this.matched) {
         match = pattern
         break
+      }
+
+      if (value instanceof Error) {
+        this.matched = lhs === Error || (lhs instanceof Error && lhs === value)
+        if (this.matched) {
+          match = pattern
+          break
+        }
+      }
+
+      if (value instanceof Promise) {
+        this.matched =
+          lhs === Promise || (lhs instanceof Promise && lhs === value)
+        if (this.matched) {
+          match = pattern
+          break
+        }
+      }
+
+      if (value === null) {
+        this.matched = lhs === null
+        if (this.matched) {
+          match = pattern
+          break
+        }
+      }
+
+      if (value === undefined) {
+        this.matched = lhs === undefined
+        if (this.matched) {
+          match = pattern
+          break
+        }
+      }
+
+      if (Array.isArray(value)) {
+        console.log("array", lhs, value)
+        if (lhs === Array) {
+          this.matched = true
+        } else if (Array.isArray(lhs)) {
+          this.matched =
+            lhs.length === value.length &&
+            lhs.every((p, i) => String(p) === String(value[i]))
+        } else {
+          continue
+        }
+
+        if (this.matched) {
+          match = pattern
+          break
+        }
+      }
+
+      if (isObject(value)) {
+        if (lhs === Object) {
+          this.matched = true
+        } else if (isObject(lhs)) {
+          const aKeys = Object.keys(value)
+          const bKeys = Object.keys(lhs)
+          this.matched =
+            aKeys.length === bKeys.length &&
+            aKeys.every((p) => String(lhs[p]) === String(value[p]))
+        } else {
+          continue
+        }
+
+        if (this.matched) {
+          match = pattern
+          console.log("obj match", pattern, value)
+          break
+        }
       }
     }
 
     if (match) {
       try {
         match[1]()
-        this.matched = true
       } catch (error: any) {
         this.error = error
       }
@@ -188,6 +133,15 @@ class PatternMatcher {
     if (this.error) fallback(this.error)
     return this
   }
+}
+
+function isObject(value: any) {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    !Array.isArray(value) &&
+    !(value instanceof Promise)
+  )
 }
 
 export function match(x: any) {
