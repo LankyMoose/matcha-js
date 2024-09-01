34b2d60564b92dc7194f6a736a158a0df6be05e6
Robby6Strings <rausten93@gmail.com>
2024-09-01 23:10:25 +1200

    remove & untrack lib/dist

--- package/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Fn, LHS, Resolved } from "./types.js";
-import { type, optional, nullable, Value, _ } from "./value.js";
-export { match, type, optional, nullable, Value, _, is };
-/**
- * @throws {Error} if no match is found
- */
-declare function match(value: any): <U extends [any, Fn<any, any>][]>(...items: U) => ReturnType<U[number][1]>;
-declare function is<T, U>(val: T, func: (val: Resolved<T>) => U): [LHS<T>, (val: Resolved<T>) => U];
